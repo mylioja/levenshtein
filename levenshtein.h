ae570915a2e205a7e8fa0ac195aa27bf2e1d8d69
Martti Ylioja <mylioja@users.noreply.github.com>
2020-08-23 20:22:11 +0200

    Improve comments

--- levenshtein.h
+++ levenshtein.h
@@ -5,15 +5,15 @@
 namespace Ylioja {
 
     //
-    //  Fast and simple version of the Wagner and Fischer Levenshtein distance algorithm.
-    //  Optimized for short (up to LEVENSHTEIN_MAX_INPUT_SIZE bytes) strings.
-    //  I initially needed the algorithm for comparing Finnish words, so even the current
-    //  limit is much higher than length of any valid word encountered in real usage.
+    //  This is my fast and simple version of the Wagner-Fischer Levenshtein distance algorithm.
+    //  It's optimized for short (up to LEVENSHTEIN_MAX_INPUT_SIZE bytes) strings.
+    //  I needed the algorithm for comparing Finnish words, so even the current size limit
+    //  is much higher than really needed in my use-case.
     //
     
-    //  This function computes Levenshtein distance between two strings.
+    //  Computes Levenshtein distance between two strings.
     //
-    //  The string arguments are treated as byte strings with one charater per byte.
+    //  The string arguments are treated as byte strings with each byte counted as a character.
     //
     //  If shorter of the two arguments is longer than LEVENSHTEIN_MAX_INPUT_SIZE,
     //  the function returns -1 to indicate an error.
